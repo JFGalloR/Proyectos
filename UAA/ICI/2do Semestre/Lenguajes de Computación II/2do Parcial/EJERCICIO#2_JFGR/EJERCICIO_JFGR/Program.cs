04ebceebbb4d86be3ec00cabe45b0427325da59c
JFGalloR <xjfgrxx@gmail.com>
2025-04-21 20:45:39 -0600

    Add files via upload

---
+++ UAA/ICI/2do Semestre/Lenguajes de Computación II/2do Parcial/EJERCICIO#2_JFGR/EJERCICIO_JFGR/Program.cs
@@ -0,0 +1,49 @@
+﻿/*=============================================
+ EJERCICIO #2. NÚMERO MAYOR.
+ ----------------------------------------------
+ OBJETIVO:
+    Realice un programa en C#, en donde 
+    utilizando la instrucción If o If - else, 
+    indicar cual numero es mayor de dos números 
+    dados.
+ ----------------------------------------------
+ Juan Francisco Gallo Ramírez
+ 23 de Febrero de 2023
+ I.C.I.
+=============================================*/
+
+using System;
+
+namespace EJERCICIO_JFGR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("=================== NÚMERO MAYOR ===================");
+            Console.WriteLine();
+            Console.WriteLine(" > Ingresa el número #1: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" > Ingresa el número #2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------");
+            if (num1 > num2)
+            {
+                Console.WriteLine(" >> El número {0} es mayor que el número {1}.", num1, num2);
+            }
+            else
+            if (num1 < num2)
+            {
+                Console.WriteLine(" >> El número {1} es mayor que el número {0}.", num1, num2);
+            }
+            else
+            {
+                Console.WriteLine(" >> El número {0} es igual que el número {1}.", num1, num2);
+            }
+            Console.WriteLine("====================================================");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UAA/ICI/2do Semestre/Lenguajes de Computación II/2do Parcial/EJERCICIO#4_JFGR/Program.cs
@@ -0,0 +1,59 @@
+﻿/*===========================================================
+  EJERCICIO. Vector de Números.
+ ----------------------------------------------------------
+  OBJETIVO.
+    Elaborar un programa de un vector de 10 elementos o 
+    pedirle al usuario de cuantos elementos desea el vector, 
+    crearlo, y encontrar el numero mayor y el menor y su 
+    posición.
+ ----------------------------------------------------------
+    Juan Francisco Gallo Ramírez
+    23 de Febrero de 2023
+    I.C.I.
+===========================================================*/
+using System;
+namespace VectorMayorMenor;
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("======================= VECTOR DE n ELEMENTOS =======================\n");
+        Console.Write("\t> Ingrese la cantidad de elementos del vector: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        int[] vector = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("\t> Ingrese el elemento {0}: ", i + 1);
+            vector[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int mayor = vector[0];
+        int posMayor = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (vector[i] > mayor)
+            {
+                mayor = vector[i];
+                posMayor = i+1;
+            }
+        }
+
+        int menor = vector[0];
+        int posMenor = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (vector[i] < menor)
+            {
+                menor = vector[i];
+                posMenor = i+1;
+            }
+        }
+        Console.WriteLine("\n---------------------------------------------------------------------\n");
+        Console.WriteLine("\t> El número mayor del vector es: {0} (posición {1})", mayor, posMayor);
+        Console.WriteLine("\t> El número menor del vector es: {0} (posición {1})", menor, posMenor);
+
+        Console.ReadKey();
+    }
+}

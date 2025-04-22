9f1cbe2409cb2bd95e7be11080b9f75edec47302
JFGalloR <xjfgrxx@gmail.com>
2025-04-21 21:31:26 -0600

    Add files via upload

---
+++ UAA/ICI/3er Semestre/Lenguajes de Computación III/Programas/ExamenUnidadI_JFGR/src/examenunidadi_jfgr/ExamenUnidadI_JFGR.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenunidadi_jfgr;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author xxjfg
+ */
+public class ExamenUnidadI_JFGR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Juego j = new Juego();
+        j.show();
+    }
+    
+}
